1b3c4358f9186c4b090ba169b47a1a8abc089ba1
arish-mhrzj <arish.mhrzn2004@gmail.com>
2024-09-11 09:29:10 +0545

    added some news components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import './App.css';
-import React, { Component } from 'react'
+import "./App.css";
+import React, { Component } from "react";
+import NavBar from "./components/NavBar";
+import News from "./components/News";
 export default class App extends Component {
   render() {
     return (
-      <div>   
-        Hello my first class based component 
+      <div>
+        <NavBar />
+        <News />
       </div>
-    )
+    );
   }
 }
-
-
-
-
-
