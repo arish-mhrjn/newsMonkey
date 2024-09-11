6130c78ca155fc5618c4153805167c2cf5d1dfd4
arish-mhrzj <arish.mhrzn2004@gmail.com>
2024-09-11 14:34:42 +0545

    API use and api calls

--- src/components/News.js
+++ src/components/News.js
@@ -39,6 +39,15 @@ export class News extends Component {
       loading: false,
     };
   }
+
+  async componentDidMount() {
+    let url =
+      "https://newsapi.org/v2/top-headlines?country=us&apiKey=322251ffb4284c9ea65cc27965d8dd79";
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({ articles: parsedData.articles });
+  }
+
   render() {
     return (
       <div className="container my-5">
@@ -48,8 +57,10 @@ export class News extends Component {
             return (
               <div className="col-md-4" key={element.url}>
                 <NewsItem
-                  title={element.title.slice(0, 45)}
-                  description={element.description.slice(0, 88)}
+                  title={element.title ? element.title.slice(0, 45) : ""}
+                  description={
+                    element.description ? element.description.slice(0, 88) : ""
+                  }
                   imageUrl={element.urlToImage}
                   newsUrl={element.url}
                 />
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,7 +6,15 @@ export class NewsItem extends Component {
     return (
       <div className="my-3">
         <div className="card" style={{ width: "18rem" }}>
-          <img src={imageUrl} className="card-img-top" alt="..." />
+          <img
+            src={
+              imageUrl
+                ? imageUrl
+                : "https://dims.apnews.com/dims4/default/a05105a/2147483647/strip/true/crop/1852x1042+0+96/resize/1440x810!/quality/90/?url=https%3A%2F%2Fassets.apnews.com%2Fee%2F29%2Fc05363225ded8e75b2f33f074fb6%2F56f2e0f645c24a359b5b639884a79ee4"
+            }
+            className="card-img-top"
+            alt="..."
+          />
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
             <p className="card-text">{description}...</p>
