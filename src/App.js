7373ea5b11dfd1d0c41c338d36a30f1db39c7e0b
arish-mhrzj <arish.mhrzn2004@gmail.com>
2024-09-12 18:31:36 +0545

    Added nav headers and default props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import "./App.css";
-import React, { Component } from "react";
-import NavBar from "./components/NavBar";
-import News from "./components/News";
+import './App.css';
+import React, { Component } from 'react';
+import NavBar from './components/NavBar';
+import News from './components/News';
 export default class App extends Component {
   render() {
     return (
       <div>
         <NavBar />
-        <News pageSize={5} />
+        <News pageSize={5} country="us" category="science" />
       </div>
     );
   }
