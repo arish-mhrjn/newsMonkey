60a2d7eab98e9f8e791dcf98c599847decf4bdfb
arish-mhrzj <arish.mhrzn2004@gmail.com>
2024-09-12 20:04:17 +0545

    Navbar route updated and key applied

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,68 +1,75 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class NavBar extends Component {
   render() {
     return (
       <div>
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-          <a className="navbar-brand" href="/">
+          <Link className="navbar-brand" to="/">
             NewsMonkey
-          </a>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
 
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mr-auto">
               <li className="nav-item active">
-                <a className="nav-link" href="/">
-                  Home <span className="sr-only">(current)</span>
-                </a>
+                <Link className="nav-link" to="/general">
+                  General <span className="sr-only">(current)</span>
+                </Link>
               </li>
+
               <li className="nav-item">
-                <a className="nav-link" href="/about">
-                  About
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/business">
+                <Link className="nav-link" to="/business">
                   Business
-                </a>{' '}
+                </Link>{' '}
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/entertainment">
+                <Link className="nav-link" to="/entertainment">
                   Entertainment
-                </a>{' '}
+                </Link>{' '}
               </li>
+
               <li className="nav-item">
-                <a className="nav-link" href="/general">
-                  General
-                </a>{' '}
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/health">
+                <Link className="nav-link" to="/health">
                   Health
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/science">
+                <Link className="nav-link" to="/science">
                   Science
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/sports">
+                <Link className="nav-link" to="/sports">
                   Sports
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/technology">
+                <Link className="nav-link" to="/technology">
                   Technology
-                </a>
+                </Link>
               </li>
             </ul>
             <form className="form-inline my-2 my-lg-0">
-              <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-              <button className="btn btn-outline-success my-2 my-sm-0" type="submit">
+              <input
+                className="form-control mr-sm-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button
+                className="btn btn-outline-success my-2 my-sm-0"
+                type="submit">
                 Search
               </button>
             </form>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,31 +1,32 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 export class NewsItem extends Component {
   render() {
     let { title, description, imageUrl, newsUrl } = this.props;
     return (
       <div className="my-3">
-        <div className="card" style={{ width: "18rem" }}>
+        <div className="card">
           <img
             src={
               imageUrl
                 ? imageUrl
-                : "https://dims.apnews.com/dims4/default/a05105a/2147483647/strip/true/crop/1852x1042+0+96/resize/1440x810!/quality/90/?url=https%3A%2F%2Fassets.apnews.com%2Fee%2F29%2Fc05363225ded8e75b2f33f074fb6%2F56f2e0f645c24a359b5b639884a79ee4"
+                : 'https://dims.apnews.com/dims4/default/a05105a/2147483647/strip/true/crop/1852x1042+0+96/resize/1440x810!/quality/90/?url=https%3A%2F%2Fassets.apnews.com%2Fee%2F29%2Fc05363225ded8e75b2f33f074fb6%2F56f2e0f645c24a359b5b639884a79ee4'
             }
             className="card-img-top"
             alt="..."
           />
           <div className="card-body">
-            <h5 className="card-title">{title}...</h5>
-            <p className="card-text">{description}...</p>
-            <a
-              href={newsUrl}
-              target="_blank"
-              rel="noreferrer"
-              className="btn btn-sm btn-dark"
-            >
-              Read more
-            </a>
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <div style={{ textAlign: 'end' }}>
+              <a
+                href={newsUrl}
+                target="_blank"
+                rel="noreferrer"
+                className="btn btn-sm btn-dark">
+                Read more
+              </a>
+            </div>
           </div>
         </div>
       </div>
